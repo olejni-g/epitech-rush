0725e4ebc837acd59bd3cbfee95770b9cf703b26
closingin <closingin@users.noreply.github.com>
2014-05-10 15:23:04 +0200

    added options.c

---
+++ epic_editor/options.c
@@ -0,0 +1,98 @@
+/*
+** options.c for epic_editor in /home/closingin/documents/projects/epitech/rush_rpg/epic_editor
+**
+** Made by      Rémi WEISLINGER
+** Login        weisli_r
+**
+** Started on   Sat May 10 13:36:00 2014 weisli_r
+** Update       Sat May 10 15:17:12 2014 weisli_r
+*/
+
+#include <stdlib.h>
+#include <prototypes.h>
+#include <my.h>
+
+int		create_header()
+{
+  int	fd;
+  char	*s;
+
+  while (42)
+  {
+	my_putstr("   Enter your header : ");
+	s = my_getline(0);
+	if (my_strcmp(s, "0"))
+	  exit(my_puts("   Goodbye !"));
+	else if (my_strcmp(s, "1"))
+	{
+	  my_putstr("\n   ");
+	  my_puts("Syntax            : GAMEFILE|BOSS_ROOM|SPAWN_ROOM");
+	}
+	else
+	{
+	  if ((fd = get_header(s)) != -1)
+	  {
+		my_puts("   Created the gamefile\n");
+		return (fd);
+	  }
+	}
+  }
+  return (0);
+}
+
+void	create_champ(int fd)
+{
+  char	*s;
+
+  while (42)
+  {
+	my_putstr("   Enter your settings : ");
+	s = my_getline(0);
+	if (my_strcmp(s, "0"))
+	  exit(my_puts("   Goodbye !"));
+	else if (my_strcmp(s, "1"))
+	{
+	  my_putstr("\n   ");
+	  my_puts("Syntax            : NAME|TYPE|HP|SPE|SPEED|DEG|WEAPON|ARMOR");
+	}
+	else
+	{
+	  if ((fd = get_header(s)) != -1)
+	  {
+		my_puts("   Champion added !\n");
+	  }
+	}
+  }
+}
+
+void create_monster(int fd)
+{
+  char	*s;
+
+  while (42)
+  {
+	my_putstr("   Enter your settings : ");
+	s = my_getline(0);
+	if (my_strcmp(s, "0"))
+	  exit(my_puts("   Goodbye !"));
+	else if (my_strcmp(s, "1"))
+	{
+	  my_putstr("\n   ");
+	  my_puts("Syntax            : TYPE|HP|SPE|SPEED|DEG|WEAPON|ARMOR");
+	}
+	else
+	{
+	  if ((fd = get_monster(s)) != -1)
+	  {
+		my_puts("   Monster added !\n");
+	  }
+	}
+  }
+}
+
+void create_room(int fd)
+{
+  if (fd)
+  {
+  }
+}
