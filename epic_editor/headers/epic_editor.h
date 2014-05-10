f1b1571be854f30184a66b5b3901aace8690b43d
hennuy_w <hennuy_w@epitech.eu>
2014-05-10 10:36:00 +0200

    lol

--- epic_editor/headers/epic_editor.h
+++ epic_editor/headers/epic_editor.h
@@ -1,5 +1,5 @@
 /*
-** headers/epic_editor.h for headers in /home/closingin/documents/projects/epitech/rush_rpg/epic_editor/headers
+** headers/epic_editor.h for headers
 **
 ** Made by      Rémi WEISLINGER
 ** Login        weisli_r
@@ -13,3 +13,39 @@
 #define RED  "\033[0;31m"
 #define GREN "\033[0;32m"
 #define BLUE "\033[0;36m"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../libmy/my.h"
+
+typedef struct champ_s
+{
+  char		*name;
+  char		*type;
+  int		hp;
+  int		spe;
+  int		speed;
+  int		dmg;
+  char		*weapon;
+  char		*armor;
+}champ_t;
+
+typedef struct monster_s
+{
+  char		*type;
+  int		hp;
+  int		spe;
+  int		speed;
+  int		dmg;
+  char		*weapon;
+  char		*armor;
+}monster_t;
+
+typedef struct room_s
+{
+  char		*name;
+  char		*adv;
+  char		*connect;
+  char		*monsters;
+}room_t;
