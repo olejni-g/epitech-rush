05f93c25d049a9e47b7302b38e9b2016df26482b
hennuy_w <hennuy_w@epitech.eu>
2014-05-10 11:29:40 +0200

    parce que j'avais besoin

--- epic_editor/get_header.c
+++ epic_editor/get_header.c
@@ -5,7 +5,7 @@
 ** Login   <wilfried@epitech.net>
 ** 
 ** Started on  Sat May 10 10:38:45 2014 HENNUYER WILFRIED
-** Last update Sat May 10 11:11:21 2014 HENNUYER WILFRIED
+** Last update Sat May 10 11:28:09 2014 HENNUYER WILFRIED
 */
 
 #include <epic_editor.h>
@@ -14,9 +14,12 @@ int		write_header(t_header *header)
 {
   int		fd;
 
-  fd = open(header->name, O_RDWR | O_CREAT, 00644);
+  if ((fd = open(header->name, O_RDWR | O_CREAT, 00644)) == 0)
+    {
+      my_puts("Fail open, try again.\n");
+      return (-1);
+    }
   write(fd, MAGIC_NUMBER, 3);
-  write(fd, 123, 8);
   write(fd, 1, 8);
   write(fd, strlen(header->name), 8);
   write(fd, header->name, strlen(header->name));
@@ -31,13 +34,15 @@ int		write_header(t_header *header)
 
 int		get_header(char *str)
 {
-  int		fd;
   char		**line;
-  header_t	header;
+  t_header	header;
 
   line = my_str_to_wordtab(str, "|");
   if (tab_len(line) != 3)
-    return (-1);
+    {
+      my_puts(ERROR_HEADER);
+      return (-1);
+    }
   header.name = line[1];
   header.win = line[2];
   header.spawn = line[3];
